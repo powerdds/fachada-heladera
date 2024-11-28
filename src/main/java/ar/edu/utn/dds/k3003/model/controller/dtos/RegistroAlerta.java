a9a6484ce09102f2e9eb28c344e3606b573d4c20
MoGoX123 <emogolloncuenca@frba.utn.edu.ar>
2024-11-28 11:02:48 -0300

    Formatear fecha de AlertaDTO mas legible

--- src/main/java/ar/edu/utn/dds/k3003/model/controller/dtos/RegistroAlerta.java
+++ src/main/java/ar/edu/utn/dds/k3003/model/controller/dtos/RegistroAlerta.java
@@ -1,5 +1,6 @@
 package ar.edu.utn.dds.k3003.model.controller.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -10,6 +11,10 @@ public class RegistroAlerta {
 
 
     private final TipoAlerta tipoAlerta;
+    @JsonFormat(
+            shape = JsonFormat.Shape.STRING,
+            pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'"
+    )
     private final LocalDateTime fechaMedicion;
 
     public RegistroAlerta(TipoAlerta tipoAlerta, LocalDateTime fechaMedicion) {
